78df10ed39e3f708db9fc27af03cbe8d280cd460
PineGolden311 <software.developer112@gmail.com>
2019-02-28 06:48:06 +0400

    main interface

--- src/App.js
+++ src/App.js
@@ -2,12 +2,52 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import TodosList from "./components/todos-list";
+import EditTodo from "./components/edit-todo";
+import CreateTodo from "./components/create-todo";
+
+import logo from "./logo.png";
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a
+              className="navbar-brand"
+              href="https://codingthesmartway.com"
+              target="_blank"
+            >
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="CodingTheSmartWay.com"
+              />
+            </a>
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br />
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
       </Router>
     );
